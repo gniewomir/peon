952e8cd1a2c8aa2ad8377aca159378bbb913720a
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2026-04-16 21:55:32 +0200

    chore: add linting for scripts

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,19 @@
 import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ['dist/**', 'node_modules/**', 'scripts/**'],
+    files: ['scripts/**/*.{js,cjs,mjs,ts,cts,mts}'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**', 'data/**'],
   },
   eslintConfigPrettier,
 );
